09d784ccd45e95f5eb3ddd5bbf1296ebaae5ba53
Oishe_khan <oisheekhan90@gmail.com>
2025-03-12 13:36:14 +0600

    update

--- src/app/Component/Main/Contact.jsx
+++ src/app/Component/Main/Contact.jsx
@@ -1,161 +1,228 @@
-import React from 'react'
+import React from "react";
+import emailjs from "@emailjs/browser";
+import { useState } from "react";
+import Swal from "sweetalert2"; // Import SweetAlert
 
 export default function Contact() {
-    return (
-        <section>
-            {/* Container */}
-            <div className="mx-auto w-full max-w-7xl px-5 py-16 md:px-10 md:py-20">
-                {/* Component */}
-                <div className="grid items-center gap-8 sm:gap-20 lg:grid-cols-2">
-                    <div className="max-w-3xl">
-                        {/* Title */}
-                        <h2 className="mb-2 text-center md:text-left text-3xl font-bold md:text-5xl">
-                            Your Partnership Starts Here!
-                        </h2>
-                        <p className="mb-8 mt-4 text-center md:text-left text-sm text-gray-500 sm:text-base md:mb-12 lg:mb-16">
-                            Reach out to us for inquiries, product details, or collaborations.
-                        </p>
-                        {/* Testimonial */}
-                        <div className="mb-4 flex items-center text-orange-500">
-                            <svg
-                                className="h-8 w-8"
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="100%"
-                                height="100%"
-                                preserveAspectRatio="xMidYMid meet"
-                                viewBox="0 0 24 24"
-                            >
-                                <path
-                                    fill="currentColor"
-                                    d="m12 17.27l4.15 2.51c.76.46 1.69-.22 1.49-1.08l-1.1-4.72l3.67-3.18c.67-.58.31-1.68-.57-1.75l-4.83-.41l-1.89-4.46c-.34-.81-1.5-.81-1.84 0L9.19 8.63l-4.83.41c-.88.07-1.24 1.17-.57 1.75l3.67 3.18l-1.1 4.72c-.2.86.73 1.54 1.49 1.08l4.15-2.5z"
-                                ></path>
-                            </svg>
-                            <svg
-                                className="h-8 w-8"
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="100%"
-                                height="100%"
-                                preserveAspectRatio="xMidYMid meet"
-                                viewBox="0 0 24 24"
-                            >
-                                <path
-                                    fill="currentColor"
-                                    d="m12 17.27l4.15 2.51c.76.46 1.69-.22 1.49-1.08l-1.1-4.72l3.67-3.18c.67-.58.31-1.68-.57-1.75l-4.83-.41l-1.89-4.46c-.34-.81-1.5-.81-1.84 0L9.19 8.63l-4.83.41c-.88.07-1.24 1.17-.57 1.75l3.67 3.18l-1.1 4.72c-.2.86.73 1.54 1.49 1.08l4.15-2.5z"
-                                ></path>
-                            </svg>
-                            <svg
-                                className="h-8 w-8"
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="100%"
-                                height="100%"
-                                preserveAspectRatio="xMidYMid meet"
-                                viewBox="0 0 24 24"
-                            >
-                                <path
-                                    fill="currentColor"
-                                    d="m12 17.27l4.15 2.51c.76.46 1.69-.22 1.49-1.08l-1.1-4.72l3.67-3.18c.67-.58.31-1.68-.57-1.75l-4.83-.41l-1.89-4.46c-.34-.81-1.5-.81-1.84 0L9.19 8.63l-4.83.41c-.88.07-1.24 1.17-.57 1.75l3.67 3.18l-1.1 4.72c-.2.86.73 1.54 1.49 1.08l4.15-2.5z"
-                                ></path>
-                            </svg>
-                            <svg
-                                className="h-8 w-8"
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="100%"
-                                height="100%"
-                                preserveAspectRatio="xMidYMid meet"
-                                viewBox="0 0 24 24"
-                            >
-                                <path
-                                    fill="currentColor"
-                                    d="m12 17.27l4.15 2.51c.76.46 1.69-.22 1.49-1.08l-1.1-4.72l3.67-3.18c.67-.58.31-1.68-.57-1.75l-4.83-.41l-1.89-4.46c-.34-.81-1.5-.81-1.84 0L9.19 8.63l-4.83.41c-.88.07-1.24 1.17-.57 1.75l3.67 3.18l-1.1 4.72c-.2.86.73 1.54 1.49 1.08l4.15-2.5z"
-                                ></path>
-                            </svg>
-                            <svg
-                                className="h-8 w-8"
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="100%"
-                                height="100%"
-                                preserveAspectRatio="xMidYMid meet"
-                                viewBox="0 0 24 24"
-                            >
-                                <path
-                                    fill="currentColor"
-                                    d="m12 17.27l4.15 2.51c.76.46 1.69-.22 1.49-1.08l-1.1-4.72l3.67-3.18c.67-.58.31-1.68-.57-1.75l-4.83-.41l-1.89-4.46c-.34-.81-1.5-.81-1.84 0L9.19 8.63l-4.83.41c-.88.07-1.24 1.17-.57 1.75l3.67 3.18l-1.1 4.72c-.2.86.73 1.54 1.49 1.08l4.15-2.5z"
-                                ></path>
-                            </svg>
-                        </div>
-                        <p className="mb-8 max-w-lg text-sm text-gray-500 sm:text-base">
-                            "Sourav Agro Dairy and Poultry has been a game-changer for our restaurant. Their products are consistently fresh and of the highest quality, making a noticeable difference in our dishes. Our customers love the rich flavor of their butter and cheese, and the poultry is always top-notch. We couldn't ask for a better partner in delivering excellence to our diners."
-                        </p>
-                        <div className="flex">
-                            <img
-                                src="https://firebasestorage.googleapis.com/v0/b/flowspark-1f3e0.appspot.com/o/Tailspark%20Images%2FPlaceholder%20Image.svg?alt=media&token=375a1ea3-a8b6-4d63-b975-aac8d0174074"
-                                alt=""
-                                className="mr-4 inline-block h-16 w-16 rounded-full object-cover"
-                            />
-                            <div className="flex flex-col">
-                                <h6 className="text-base font-bold">XYZ</h6>
-                                <p className="text-sm text-gray-500">Customer</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="mx-auto max-w-xl bg-gray-100 p-8 text-center">
-                        <h3 className="text-2xl font-bold md:text-3xl">Get a free quote</h3>
-                        <p className="mx-auto mb-6 mt-4 max-w-lg text-sm text-gray-500 lg:mb-8">
-                            We’d love to hear from you! Let’s discuss how we can meet your needs.
-                        </p>
-                        {/* Form */}
-                        <form
-                            className="mx-auto mb-4 max-w-sm text-left"
-                            name="wf-form-password"
-                            method="get"
-                        >
-                            <div>
-                                <label htmlFor="name-2" className="mb-1 font-medium">
-                                    Your Name
-                                </label>
-                                <input
-                                    type="text"
-                                    className="mb-4 block h-9 w-full rounded-md border border-solid border-black px-3 py-6 pl-4 text-sm text-black"
-                                />
-                            </div>
-                            <div className="mb-2">
-                                <label htmlFor="name-2" className="mb-1 font-medium">
-                                    Email Address
-                                </label>
-                                <input
-                                    type="text"
-                                    className="mb-4 block h-9 w-full rounded-md border border-solid border-black px-3 py-6 pl-4 text-sm text-black"
-                                />
-                            </div>
-                            <div className="mb-2">
-                                <label htmlFor="name-2" className="mb-1 font-medium">
-                                    Subject
-                                </label>
-                                <input
-                                    type="text"
-                                    className="mb-4 block h-9 w-full rounded-md border border-solid border-black px-3 py-6 pl-4 text-sm text-black"
-                                />
-                            </div>
-                            <div className="mb-5 md:mb-6 lg:mb-8">
-                                <label htmlFor="field-3" className="mb-1 font-medium">
-                                    Message
-                                </label>
-                                <textarea
-                                    placeholder=""
-                                    maxLength="5000"
-                                    name="field"
-                                    className="mb-2.5 block h-auto min-h-32 w-full rounded-md border border-solid border-black p-3 text-sm text-black"
-                                ></textarea>
-                            </div>
-                            <input
-                                type="submit"
-                                value="Get free quote"
-                                className="inline-block w-full cursor-pointer rounded-md bg-black px-6 py-3 text-center font-semibold text-white"
-                            />
-                        </form>
-                    </div>
-                </div>
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    const templateParams = {
+      name: name,
+      email: email,
+      subject: subject,
+      message: message,
+    };
+
+    emailjs
+      .send(
+        "service_73bnpc7",
+        "template_zljcnwi",
+        templateParams,
+        "WWL1w5R_PgBdaTyn9"
+      )
+      .then(
+        (result) => {
+          console.log("Email sent:", result.text);
+          // Show success SweetAlert
+          Swal.fire({
+            title: "Success!",
+            text: "Your message has been sent to the admin!",
+            icon: "success",
+            confirmButtonText: "Ok",
+          });
+          setEmail("");
+          setMessage("");
+        },
+        (error) => {
+          console.error("Error:", error.text);
+          // Show error SweetAlert
+          Swal.fire({
+            title: "Error!",
+            text: "Failed to send email.",
+            icon: "error",
+            confirmButtonText: "Try Again",
+          });
+        }
+      );
+  };
+
+  return (
+    <section>
+      {/* Container */}
+      <div className="mx-auto w-full max-w-7xl px-5 py-16 md:px-10 md:py-20">
+        {/* Component */}
+        <div className="grid items-center gap-8 sm:gap-20 lg:grid-cols-2">
+          <div className="max-w-3xl">
+            {/* Title */}
+            <h2 className="mb-2 text-center md:text-left text-3xl font-bold md:text-5xl">
+              Your Partnership Starts Here!
+            </h2>
+            <p className="mb-8 mt-4 text-center md:text-left text-sm text-gray-500 sm:text-base md:mb-12 lg:mb-16">
+              Reach out to us for inquiries, product details, or collaborations.
+            </p>
+            {/* Testimonial */}
+            <div className="mb-4 flex items-center text-orange-500">
+              <svg
+                className="h-8 w-8"
+                xmlns="http://www.w3.org/2000/svg"
+                width="100%"
+                height="100%"
+                preserveAspectRatio="xMidYMid meet"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="m12 17.27l4.15 2.51c.76.46 1.69-.22 1.49-1.08l-1.1-4.72l3.67-3.18c.67-.58.31-1.68-.57-1.75l-4.83-.41l-1.89-4.46c-.34-.81-1.5-.81-1.84 0L9.19 8.63l-4.83.41c-.88.07-1.24 1.17-.57 1.75l3.67 3.18l-1.1 4.72c-.2.86.73 1.54 1.49 1.08l4.15-2.5z"
+                ></path>
+              </svg>
+              <svg
+                className="h-8 w-8"
+                xmlns="http://www.w3.org/2000/svg"
+                width="100%"
+                height="100%"
+                preserveAspectRatio="xMidYMid meet"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="m12 17.27l4.15 2.51c.76.46 1.69-.22 1.49-1.08l-1.1-4.72l3.67-3.18c.67-.58.31-1.68-.57-1.75l-4.83-.41l-1.89-4.46c-.34-.81-1.5-.81-1.84 0L9.19 8.63l-4.83.41c-.88.07-1.24 1.17-.57 1.75l3.67 3.18l-1.1 4.72c-.2.86.73 1.54 1.49 1.08l4.15-2.5z"
+                ></path>
+              </svg>
+              <svg
+                className="h-8 w-8"
+                xmlns="http://www.w3.org/2000/svg"
+                width="100%"
+                height="100%"
+                preserveAspectRatio="xMidYMid meet"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="m12 17.27l4.15 2.51c.76.46 1.69-.22 1.49-1.08l-1.1-4.72l3.67-3.18c.67-.58.31-1.68-.57-1.75l-4.83-.41l-1.89-4.46c-.34-.81-1.5-.81-1.84 0L9.19 8.63l-4.83.41c-.88.07-1.24 1.17-.57 1.75l3.67 3.18l-1.1 4.72c-.2.86.73 1.54 1.49 1.08l4.15-2.5z"
+                ></path>
+              </svg>
+              <svg
+                className="h-8 w-8"
+                xmlns="http://www.w3.org/2000/svg"
+                width="100%"
+                height="100%"
+                preserveAspectRatio="xMidYMid meet"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="m12 17.27l4.15 2.51c.76.46 1.69-.22 1.49-1.08l-1.1-4.72l3.67-3.18c.67-.58.31-1.68-.57-1.75l-4.83-.41l-1.89-4.46c-.34-.81-1.5-.81-1.84 0L9.19 8.63l-4.83.41c-.88.07-1.24 1.17-.57 1.75l3.67 3.18l-1.1 4.72c-.2.86.73 1.54 1.49 1.08l4.15-2.5z"
+                ></path>
+              </svg>
+              <svg
+                className="h-8 w-8"
+                xmlns="http://www.w3.org/2000/svg"
+                width="100%"
+                height="100%"
+                preserveAspectRatio="xMidYMid meet"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="m12 17.27l4.15 2.51c.76.46 1.69-.22 1.49-1.08l-1.1-4.72l3.67-3.18c.67-.58.31-1.68-.57-1.75l-4.83-.41l-1.89-4.46c-.34-.81-1.5-.81-1.84 0L9.19 8.63l-4.83.41c-.88.07-1.24 1.17-.57 1.75l3.67 3.18l-1.1 4.72c-.2.86.73 1.54 1.49 1.08l4.15-2.5z"
+                ></path>
+              </svg>
             </div>
-        </section>
-    )
+            <p className="mb-8 max-w-lg text-sm text-gray-500 sm:text-base">
+              "Sourav Agro Dairy and Poultry has been a game-changer for our
+              restaurant. Their products are consistently fresh and of the
+              highest quality, making a noticeable difference in our dishes. Our
+              customers love the rich flavor of their butter and cheese, and the
+              poultry is always top-notch. We couldn't ask for a better partner
+              in delivering excellence to our diners."
+            </p>
+            <div className="flex">
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/flowspark-1f3e0.appspot.com/o/Tailspark%20Images%2FPlaceholder%20Image.svg?alt=media&token=375a1ea3-a8b6-4d63-b975-aac8d0174074"
+                alt=""
+                className="mr-4 inline-block h-16 w-16 rounded-full object-cover"
+              />
+              <div className="flex flex-col">
+                <h6 className="text-base font-bold">XYZ</h6>
+                <p className="text-sm text-gray-500">Customer</p>
+              </div>
+            </div>
+          </div>
+          <div className="mx-auto max-w-xl bg-gray-100 p-8 text-center">
+            <h3 className="text-2xl font-bold md:text-3xl">Get a free quote</h3>
+            <p className="mx-auto mb-6 mt-4 max-w-lg text-sm text-gray-500 lg:mb-8">
+              We’d love to hear from you! Let’s discuss how we can meet your
+              needs.
+            </p>
+            {/* Form */}
+            <form
+              className="mx-auto mb-4 max-w-sm text-left"
+              name="wf-form-password"
+              method="get"
+            >
+              <div>
+                <label htmlFor="name-2" className="mb-1 font-medium">
+                  Your Name
+                </label>
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className="mb-4 block h-9 w-full rounded-md border border-solid border-black px-3 py-6 pl-4 text-sm text-black"
+                />
+              </div>
+              <div className="mb-2">
+                <label htmlFor="name-2" className="mb-1 font-medium">
+                  Email Address
+                </label>
+                <input
+                  type="text"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="mb-4 block h-9 w-full rounded-md border border-solid border-black px-3 py-6 pl-4 text-sm text-black"
+                />
+              </div>
+              <div className="mb-2">
+                <label htmlFor="name-2" className="mb-1 font-medium">
+                  Subject
+                </label>
+                <input
+                  type="text"
+                  value={subject}
+                  onChange={(e) => setSubject(e.target.value)}
+                  className="mb-4 block h-9 w-full rounded-md border border-solid border-black px-3 py-6 pl-4 text-sm text-black"
+                />
+              </div>
+              <div className="mb-5 md:mb-6 lg:mb-8">
+                <label htmlFor="field-3" className="mb-1 font-medium">
+                  Message
+                </label>
+                <textarea
+                  placeholder=""
+                  maxLength="5000"
+                  name="field"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  className="mb-2.5 block h-auto min-h-32 w-full rounded-md border border-solid border-black p-3 text-sm text-black"
+                ></textarea>
+              </div>
+              <button
+                type="button"
+                onClick={sendEmail}
+                className="inline-block w-full cursor-pointer rounded-md bg-black px-6 py-3 text-center font-semibold text-white"
+              >
+                Get free quote
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
